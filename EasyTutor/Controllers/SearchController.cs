ab175dbf2df108efcdc35b32bea0a15eeb9db0be
Peter Rosser <tharax@gmail.com>
2016-09-18 16:49:22 +0700

    sanitize parameters

--- EasyTutor/Controllers/SearchController.cs
+++ EasyTutor/Controllers/SearchController.cs
@@ -56,9 +56,9 @@ namespace EasyTutor.Controllers
                 var selectQueryString = "SELECT u.name FROM users u" +
                                         "left join userstopics ut on u.userid = ut.usersid" +
                                         "left join topics t on t.id = ut.topicsid" +
-                                        "where t.name =''" + id + "''";
+                                        "where t.name = @0";
 
-                var query = db.Query(selectQueryString);
+                var query = db.Query(selectQueryString, id);
                 var objectToSerialize = new RootObject();
                 objectToSerialize.Users = new List<User>();
                 foreach (var row in query)
