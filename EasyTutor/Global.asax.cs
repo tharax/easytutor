fa29e42774c5ae5e589144b54e4b867ea311e432
Peter Rosser <tharax@gmail.com>
2016-09-19 13:12:01 +0700

    :scissors: Cutting Code (And I found a better emoticon).

--- EasyTutor/Global.asax.cs
+++ EasyTutor/Global.asax.cs
@@ -13,7 +13,6 @@ namespace EasyTutor
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
